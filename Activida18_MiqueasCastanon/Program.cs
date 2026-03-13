b4ee6493dfe4b6ab478e6295ba28940fe4a3c550
MiqueasCast <castmicah@gmail.com>
2026-03-13 10:45:35 -0600

    Agregar archivos de proyecto.

---
+++ Activida18_MiqueasCastanon/Program.cs
@@ -0,0 +1,28 @@
+﻿void Menu()
+{
+    Console.WriteLine("Menú: \n1. Registrar el estudiante \n2. Mostrar promedio general\n3. Mostrar cantidad aprobados y reprobados\n4. Salir");
+
+}
+
+int opcion = 0;
+
+do
+{
+    Menu();
+    Console.WriteLine("");
+    opcion = int.Parse(Console.ReadLine());
+
+    switch (opcion)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
+}while(opcion != 4)
